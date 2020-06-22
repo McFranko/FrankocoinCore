6fc7af4299cd9b3e2c152a6a6c78a84c90e6c3ae
McFranko <fronkles@mcfranko.com>
2020-06-22 12:03:16 -0700

    First commit

---
+++ src/main.rs
@@ -0,0 +1,67 @@
+#![allow(non_snake_case)]
+mod server;
+use std::io::Write;
+use std::io::Read;
+
+
+fn main() {
+    // Create server and make parameters
+    let server = server::TcpServer {
+        ipAddress: String::from("localhost:8888"),
+        handler: connectionHandler
+    };
+    // Start server on a new thread
+    std::thread::spawn(move || {
+        server.start();
+    });
+
+    // I had it just sleep for a minute now so I can test the server without the program just closing on me
+    std::thread::sleep(std::time::Duration::from_secs(60));
+}
+
+
+
+fn connectionHandler(mut stream: std::net::TcpStream) {
+    // Read request
+    let mut req: [u8; 512] = [0; 512];
+    let err = stream.read(&mut req);
+    // Handle errors
+    if err.is_err() {
+        eprintln!("Could not read stream");
+        return;
+    }
+
+    // Find what type of request it is
+    let reqStr = std::str::from_utf8(&req);
+
+    // I know I could probably make it a little faster by not converting it a string
+    // and just comparing the sent buffer against buffers of the keyworks below, but
+    // this is just a lot easier. Maybe someone else can do that if they really feel
+    // like it.
+
+    // Handle errors
+    if reqStr.is_err() {
+        eprintln!("Could not convert request to string");
+        return;
+    }
+    let reqSplit: Vec<&str> = reqStr.unwrap().splitn(1, '\n').collect();
+
+    match reqSplit[0] {
+        "newBlock" => {
+            // Check block signatures and PoW, if adds up execute frankolang
+        },
+        "newCodeSection" => {
+            // Check the code section sent, and if the signature and syntax is correct, add it to the unexecuted code list
+        },
+        "reqBlock" => {
+            // Find what block they are requesting and add write it to the stream
+        },
+        "reqUnexec" => {
+            // Send all unexecuted code (frankolang code)
+        },
+        "reqExec" => {
+            // Send all executed code (frankolang code)
+        }
+        &_ => println!("Could not find request")
+    }
+}
---
+++ src/server.rs
@@ -0,0 +1,33 @@
+// Will host a TCP server and bind it to ip_address and handle connections with handler
+// All connections are started on new threads
+#![allow(non_snake_case)]
+
+pub struct TcpServer {
+    pub ipAddress: std::string::String,
+    pub handler: fn(std::net::TcpStream)
+}
+
+impl TcpServer {
+    pub fn start(&self) {
+        let listener = std::net::TcpListener::bind(&self.ipAddress);
+        if !listener.is_ok() {
+            println!("Server failed to bind to ip address: \n{}Error message:\n{}", &self.ipAddress, listener.unwrap_err());
+            std::process::exit(-1);
+        }
+
+        for stream in listener.unwrap().incoming() {
+            let handler = self.handler;
+
+            // Server is prone to DDOS attacks right now, a thread pool would make sense
+            // I'll do that later. Another solution is to just dedicate one memory address
+            // to storing the amount of threads available that can be read from and written
+            // to by all the threads. I'm not sure if that would be considered
+            // memory safe by the compiler though, or even allowed by the kernel.
+
+            std::thread::spawn(move || {
+                (handler)(stream.unwrap());
+            });
+        }
+        
+    }
+}
