38204462a881cb8d309e086edf08c8cb1c32715f
McFranko <fronkles@mcfranko.com>
2020-06-22 15:58:46 -0700

    fixed an error message

--- src/server.rs
+++ src/server.rs
@@ -2,6 +2,8 @@
 // All connections are started on new threads
 #![allow(non_snake_case)]
 
+mod threadpool;
+
 pub struct TcpServer {
     pub ipAddress: std::string::String,
     pub handler: fn(std::net::TcpStream)
@@ -11,7 +13,7 @@ impl TcpServer {
     pub fn start(&self) {
         let listener = std::net::TcpListener::bind(&self.ipAddress);
         if !listener.is_ok() {
-            println!("Server failed to bind to ip address: \n{}Error message:\n{}", &self.ipAddress, listener.unwrap_err());
+            println!("Server failed to bind to ip address: \n{}\nError message:\n{}", &self.ipAddress, listener.unwrap_err());
             std::process::exit(-1);
         }
 
@@ -21,8 +23,8 @@ impl TcpServer {
             // Server is prone to DDOS attacks right now, a thread pool would make sense
             // I'll do that later. Another solution is to just dedicate one memory address
             // to storing the amount of threads available that can be read from and written
-            // to by all the threads. I'm not sure if that would be considered
-            // memory safe by the compiler though, or even allowed by the kernel.
+            // to by all the threads. I'm not sure if that would be considered memory
+            // safe by the compiler though, or even allowed by the kernel.
 
             std::thread::spawn(move || {
                 (handler)(stream.unwrap());
