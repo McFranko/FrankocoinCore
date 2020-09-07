fbcf73e5e8929e923b15ebbaa3677a2bc173a2e4
McFranko <mrelfranko@disroot.org>
2020-09-07 12:13:24 -0700

    Moved tests to src/test instead of being in the main() function

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,17 @@
 #![allow(non_snake_case)]
 extern crate md5;
 extern crate ed25519_dalek;
-extern crate rand;
 extern crate serde;
 extern crate bincode;
 
 mod frankolang;
 mod server;
-mod test;
+mod tests;
 
 use std::io::Read;
 
 fn main()
 {
-    // Need to add a config file
 
-    let server = server::TcpServer {
-        ipAddress: String::from("localhost:8888"), // Need to change IP to the config files IP
-        handler: connectionHandler
-    };
-    let serverThread = std::thread::spawn(move || {
-        server.start(50);
-    });
-
-    test::runTests();
-
-    serverThread.join().unwrap();
 }
 
-fn connectionHandler(mut socket: std::net::TcpStream)
-{
-    let mut request: [u8; 1048576] = [0; 1048576];
-
-    socket.read(&mut request).unwrap();
-}
