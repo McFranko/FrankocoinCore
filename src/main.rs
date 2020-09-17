f059e70013bd8222af700db4e5daf14d523facbb
McFranko <mrelfranko@disroot.org>
2020-09-17 13:56:30 -0700

    Added initializeFrankocoinDirectory as a test

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,56 @@ extern crate md5;
 extern crate ed25519_dalek;
 extern crate serde;
 extern crate bincode;
+extern crate dirs;
 
 mod frankolang;
 mod server;
 mod tests;
 
-use std::io::Read;
+use std::path::Path;
+use std::fs;
 
-fn main()
-{
+fn main() {
 
 }
 
+fn initializeFrankocoinDirectory()
+    -> Result<(), Box<dyn std::error::Error>>
+{
+    let dataDirPath = format!(
+        "{}/.frankocoin",
+        dirs::home_dir().unwrap().to_str().unwrap()
+    );
+    let dataDir = Path::new(&dataDirPath);
+
+    if dataDir.exists() {
+        return Ok(())
+    }
+
+    fs::create_dir(dataDir)?;
+
+    let balanceEntriesDir = format!(
+        "{}/balanceEntries",
+        dataDirPath
+    );
+    fs::create_dir(balanceEntriesDir)?;
+
+    // Left off here
+    let blocksDir = format!(
+        "{}/blocks",
+        dataDirPath
+    );
+    fs::create_dir(blocksDir);
+
+    Ok(())
+}
+
+fn clone_into_array<A, T>(slice: &[T]) -> A
+where
+    A: Default + AsMut<[T]>,
+    T: Clone,
+{
+    let mut a = A::default();
+    <A as AsMut<[T]>>::as_mut(&mut a).clone_from_slice(slice);
+    a
+}
--- src/tests/mod.rs
+++ src/tests/mod.rs
@@ -1,5 +1,13 @@
 #![allow(non_snake_case, dead_code)]
 #![cfg(test)]
 
+// In order for tests to work you must do `cargo run` first to initialize the frankocoin directory
+// (~/.frankocoin on UNIX systems)
+
+#[test]
+fn initializeFrankocoinDirectoryTest() {
+    crate::initializeFrankocoinDirectory().unwrap();
+}
+
 mod frankolangInterpreter;
 mod server;
