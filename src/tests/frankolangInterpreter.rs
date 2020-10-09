1d1dbea25bc62ffb8af3a92362d29dd7312f1634
Fronkles McFranko <mrelfranko@disroot.org>
2020-10-08 22:09:09 -0700

    Checks time of frankolang interpreter test
    Removed to_vec()

--- src/tests/frankolangInterpreter.rs
+++ src/tests/frankolangInterpreter.rs
@@ -6,7 +6,7 @@ use crate::tests::testFrankolang;
 #[test]
 pub fn frankolangInterpreter() {
     let code = testFrankolang();
-    let mut frankolangCode = frankolang::FrankolangCode::new(code.to_vec()).unwrap();
+    let mut frankolangCode = frankolang::FrankolangCode::new(&code).unwrap();
     frankolangCode.checkCode().unwrap();
     let duration = Instant::now();
     frankolangCode.executeCode().unwrap();
